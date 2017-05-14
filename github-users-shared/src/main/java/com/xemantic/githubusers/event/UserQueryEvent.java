373aa6d9ce827f6b6ad023bed1b1df54f533b049
Kazik Pogoda <morisil@xemantic.com>
2017-05-14 18:06:27 +0200

    new UserQueryEvent and UserSelectedEvent

---
+++ github-users-shared/src/main/java/com/xemantic/githubusers/event/UserQueryEvent.java
@@ -0,0 +1,43 @@
+/*
+ * github-users - lists GitHub users. Minimal app demonstrating
+ * cross-platform development (Web, Android, iOS) on top of
+ * Java to JavaScript and Java to Objective-C transpilers.
+ *
+ * Copyright (C) 2017  Kazimierz Pogoda
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.xemantic.githubusers.event;
+
+import java.util.Objects;
+
+/**
+ * Event generated when new user query is requested.
+ *
+ * @author morisil
+ */
+public class UserQueryEvent {
+
+  private final String query;
+
+  public UserQueryEvent(String query) {
+    this.query = Objects.requireNonNull(query);
+  }
+
+  public String getQuery() {
+    return query;
+  }
+
+}
