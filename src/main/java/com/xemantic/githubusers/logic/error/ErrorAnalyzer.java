f3e7ef852b5ef4b9fe6a7678d26f252f09e40623
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2017-11-11 08:47:45 +0100

    Fix missing ErrorAnalyzer interface

---
+++ src/main/java/com/xemantic/githubusers/logic/error/ErrorAnalyzer.java
@@ -0,0 +1,41 @@
+/*
+ * github-users - lists GitHub users. Minimal app demonstrating
+ * cross-platform app development (Web, Android, iOS) where core
+ * logic is shared and transpiled from Java to JavaScript and
+ * Objective-C. This project delivers core application logic.
+ *
+ * Copyright (C) 2017  Kazimierz Pogoda
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.xemantic.githubusers.logic.error;
+
+/**
+ * Analyzes {@link Throwable}s occurring in the streams of observed data.
+ * The result can be used to retry after recoverable error like temporal connection error.
+ */
+public interface ErrorAnalyzer {
+
+  /**
+   * Tells if supplied {@code throwable} is non-critical and therefore if action
+   * can possibly succeed on the next try. Examples: rate limit quota reached,
+   * connectivity problem, etc.
+   *
+   * @param throwable the error to analyze.
+   * @return the {@code true} if recoverable, {@code false} otherwise.
+   */
+  boolean isRecoverable(Throwable throwable);
+
+}
